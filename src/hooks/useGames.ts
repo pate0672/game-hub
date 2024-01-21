9d7b797b5a9fcdf51db4c733ea9a8b16fd7f2771
Rishi Patel <pate0672@algonquinlive.com>
2024-01-20 23:34:12 -0500

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,6 +15,6 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", { params: { genres: selectedGenre?.id }}, [selectedGenre?.id])
+const useGames = (selectedGenre: Genre | null, selectedPlatfrom: Platform | null) => useData<Game>("/games", { params: { genres: selectedGenre?.id, platforms: selectedPlatfrom?.id }}, [selectedGenre?.id, selectedPlatfrom?.id])
 
 export default useGames;
