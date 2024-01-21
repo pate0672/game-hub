b3cee58402c09386fd11669a010a3e1227374b31
Rishi Patel <pate0672@algonquinlive.com>
2024-01-20 21:31:36 -0500

    Highliting selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -29,6 +30,12 @@ const GenreList = ({ onSelectGenre }: Props) => {
               onClick={() => onSelectGenre(genre)}
               variant="link"
               fontSize="lg"
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              textDecor={
+                genre.id === selectedGenre?.id ? "underline" : "normal"
+              }
+              whiteSpace="normal"
+              textAlign="left"
             >
               {genre.name}
             </Button>
